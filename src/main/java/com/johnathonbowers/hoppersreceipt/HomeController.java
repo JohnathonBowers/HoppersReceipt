19a3645bc869463879a280f174f942e62bfe4904
Johnathon Bowers <johnathonbowers@gmail.com>
2022-12-15 13:13:41 -0500

    add files and code

---
+++ src/main/java/com/johnathonbowers/hoppersreceipt/HomeController.java
@@ -0,0 +1,28 @@
+package com.johnathonbowers.hoppersreceipt;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+	@GetMapping("/")
+	public String index(Model model) {
+		
+		String name = "Grace Hopper";
+		String itemName = "Copper wire";
+		double price = 5.25;
+		String description = "Metal strips. Also an illustration of nanoseconds.";
+		String vendor = "Little Things Corner Store";
+		
+		model.addAttribute("name", name);
+		model.addAttribute("itemName", itemName);
+		model.addAttribute("price", price);
+		model.addAttribute("description", description);
+		model.addAttribute("vendor", vendor);
+		
+		return "index.jsp";
+	}
+	
+}
